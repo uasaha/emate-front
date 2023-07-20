de8b0400c5c4805aa0b3916f0353a72d67ec901e
운석 <istana000@gmail.com>
2023-07-20 11:38:29 +0900

    CI/CD Test

--- src/main/java/me/emate/matefront/hello/Hello.java
+++ src/main/java/me/emate/matefront/hello/Hello.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping
     public String hello() {
-        return "Hello, My World!";
+        return "Hello, RY World!";
     }
 }
