68912fe9455defaf242a5ab177ef5a3a324c5817
운석 <istana000@gmail.com>
2023-07-23 21:35:08 +0900

    [Feat] 메인뷰 추가

---
+++ src/main/java/me/emate/matefront/main/MainController.java
@@ -0,0 +1,14 @@
+package me.emate.matefront.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public String mainView() {
+        return "main/main";
+    }
+}
