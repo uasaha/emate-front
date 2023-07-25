e084c7d7030009be8657423f5156e482835df155
운석 <istana000@gmail.com>
2023-07-25 21:02:22 +0900

    [Feat] main page sidebar 제작
    백서버 연결

---
+++ src/main/java/me/emate/matefront/config/ToBackConfig.java
@@ -0,0 +1,16 @@
+package me.emate.matefront.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ToBackConfig {
+
+    @Value("${emate.back.url}")
+    private String backUrl;
+
+    public String getBackUrl() {
+        return this.backUrl;
+    }
+}
